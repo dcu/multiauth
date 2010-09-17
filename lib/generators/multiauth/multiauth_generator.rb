632f5e1f06efc92a5a605c3192c5d42b0c697719
David A. Cuadrado <krawek@gmail.com>
2010-09-16 20:16:52 -0500

    fix generator
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- lib/generators/multiauth/multiauth_generator.rb
+++ lib/generators/multiauth/multiauth_generator.rb
@@ -2,18 +2,23 @@ module Multiauth
   module Generators
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../../templates", __FILE__)
+      namespace 'multiauth:install'
       desc "initializes the required files"
 
-      def copy_multiauth_js
+      def copy_js
         copy_file "multiauth.js", "public/javascripts/"
       end
 
-      def copy_openid_selector
+      def copy_openid
         copy_directory "openid-realselector", "public/javascripts/openid-realselector"
       end
 
-      def copy_twitter_initializer
+      def copy_twitter
         copy_file "devise_twitter.rb", "config/initializers/devise_twitter.rb"
+      end
+
+      def show_readme
+        readme "README"
       end
 
       protected
