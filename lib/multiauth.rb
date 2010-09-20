d7fc5ec03853610603db27eb7c0babcf5ea7318a
David A. Cuadrado <krawek@gmail.com>
2010-09-19 22:44:37 -0500

    reorder providers
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- lib/multiauth.rb
+++ lib/multiauth.rb
@@ -6,36 +6,41 @@ module Multiauth
     'Google' => {
       :url => 'https://www.google.com/accounts/o8/id'
     },
-    'Yahoo' => {
-      :url => 'http://yahoo.com/'
-    },
     'Twitter' => {
       :url => '/users/sign_in/twitter',
       :real => true
-    },
-    'Launchpad' => {
-      :url => 'https://launchpad.net/~{user_name}'
     },
     'Facebook' => {
       :url => lambda { oauth_authorize_url(resource_name, :facebook) },
       :real => true
     },
-    'OpenID' => {
-      :url => '{user_name}'
+    'Yahoo' => {
+      :url => 'http://yahoo.com/'
+    },
+    'AOL' => {
+      :url => 'http://openid.aol.com/{user_name}'
+    },
+    'MySpace' => {
+      :url => 'http://www.myspace.com/{user_name}'
     },
     'Github' => {
       :url => lambda { oauth_authorize_url(resource_name, :github) },
       :real => true
     },
-    'AOL' => {
-      :user_name_txt => 'screen:name',
-      :url => 'http://openid.aol.com/{user_name}'
-    },
     'MyOpenID' => {
       :url => 'http://{user_name}.myopenid.com/'
     },
+    'Wordpress' => {
+      :url => 'http://{user_name}.wordpress.com/'
+    },
+    'Blogger' => {
+      :url => 'http://{user_name}.blogspot.com/'
+    },
     'Flickr' => {
       :url => 'http://flickr.com/{user_name}/'
+    },
+    'Launchpad' => {
+      :url => 'https://launchpad.net/~{user_name}'
     },
     'Vidoop' => {
       :url => 'http://{user_name}.myvidoop.com/'
@@ -46,20 +51,14 @@ module Multiauth
     'Technorati' => {
       :url => 'http://technorati.com/people/technorati/{user_name}/'
     },
-    'Wordpress' => {
-      :url => 'http://{user_name}.wordpress.com/'
-    },
-    'Blogger' => {
-      :url => 'http://{user_name}.blogspot.com/'
-    },
     'Verisign' => {
       :url => 'http://{user_name}.pip.verisignlabs.com/'
     },
     'LiveJournal' => {
       :url => 'http://{user_name}.livejournal.com'
     },
-    'MySpace' => {
-      :url => 'http://www.myspace.com/{user_name}'
+    'OpenID' => {
+      :url => '{user_name}'
     }
   }
 end
