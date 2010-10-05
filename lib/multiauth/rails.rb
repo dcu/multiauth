b3903bb26af20d7937af84ad02233c8e88844baf
David A. Cuadrado <krawek@gmail.com>
2010-10-05 15:34:39 -0500

    make it work in dev env
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- lib/multiauth/rails.rb
+++ lib/multiauth/rails.rb
@@ -4,15 +4,13 @@ ActionController::Base.append_view_path File.expand_path("../../../app/views", _
 module Multiauth
   class Engine < ::Rails::Engine
     initializer "multiauth" do |app|
-      ApplicationController.send(:include, Multiauth::Helpers)
-
       config_file = Rails.root+"config/auth_providers.yml"
       providers = YAML::load(ERB.new(File.read(config_file)).result)
       if providers[Rails.env].nil?
         raise ArgumentError, "cannot find section for #{Rails.env} environment in #{config_file}"
       end
 
-      Multiauth.providers = providers[Rails.env]}
+      Multiauth.providers = providers[Rails.env]
 
       require 'omniauth/openid'
       require 'openid/store/filesystem'
@@ -27,6 +25,10 @@ module Multiauth
           provider provider.downcase.to_sym, config["id"], config["token"]
         end
       end
+    end
+
+    config.to_prepare do
+      ApplicationController.send(:include, Multiauth::Helpers)
     end
   end
 end
